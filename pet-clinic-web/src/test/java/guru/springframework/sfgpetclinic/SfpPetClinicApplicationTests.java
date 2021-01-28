50f2b4b4b096ee2b55ea40745e35247d8964c1ac
Frank Jobst <fajobst@gmail.com>
2021-01-27 19:40:34 -0600

    added changes

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfpPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfpPetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframework.sfppetclinic;
+package guru.springframework.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
