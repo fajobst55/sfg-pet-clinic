394f99da8ba810926ded0b014724848e9e99be59
Frank Jobst <fajobst@gmail.com>
2021-01-31 13:44:22 -0600

    added specialities, added to vet. closes #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,6 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+    private Set<Speciality> specialitys;
 
+    public Set<Speciality> getSpecialitys() {
+        return specialitys;
+    }
+
+    public void setSpecialitys(Set<Speciality> specialitys) {
+        this.specialitys = specialitys;
+    }
 }
