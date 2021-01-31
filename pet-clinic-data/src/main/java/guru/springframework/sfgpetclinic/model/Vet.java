078687d6066434b0c926e6ebfca678520eb6779a
Frank Jobst <fajobst@gmail.com>
2021-01-31 16:03:24 -0600

    added Pets and Contact info to owners on startup. closes #33

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,10 +1,11 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Vet extends Person {
 
-    private Set<Speciality> specialitys;
+    private Set<Speciality> specialitys = new HashSet<>();
 
     public Set<Speciality> getSpecialitys() {
         return specialitys;
